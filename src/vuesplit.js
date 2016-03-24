126293787b538f2698a45509990b65917840279b
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-24 18:01:44 +0100

    Moved in initial code

---
+++ src/vuesplit.js
@@ -0,0 +1,212 @@
+var fs = require('fs');
+var through = require('through2');
+var parse5 = require('parse5');
+var deindent = require('de-indent');
+var File = require('vinyl');
+var postcss = require('postcss');
+var series = require("async/series");
+var posthtml = require('posthtml');
+var posthtmlCssModules = require('posthtml-css-modules');
+var templateValidate = require('vue-template-validator');
+var htmlMinifier = require('html-minifier');
+
+
+// required for Vue 1.0 shorthand syntax
+var templateMinifyOptions = {
+  customAttrSurround: [[/@/, new RegExp('')], [/:/, new RegExp('')]],
+  collapseWhitespace: true,
+  removeComments: true,
+  collapseBooleanAttributes: true,
+  removeAttributeQuotes: true,
+  // this is disabled by default to avoid removing
+  // "type" on <input type="text">
+  removeRedundantAttributes: false,
+  useShortDoctype: true,
+  removeEmptyAttributes: true,
+  removeOptionalTags: true
+}
+
+function convertFragmentIntoNodeMap(fragment)
+{
+  var nodes = {};
+  fragment.childNodes.forEach(function(child) {
+    // Ignore text (typically just white space) and comment nodes
+    if (child.nodeName === "#text" || child.nodeName === "#comment") {
+      return;
+    }
+
+    var content = deindent(parse5.serialize(child.content || child)).trim();
+    nodes[child.nodeName] = content;
+  })
+
+  return nodes;
+}
+
+function getContentFromNode(node) {
+  return deindent(parse5.serialize(node.content || node));
+}
+
+function cleanTemplateText(text) {
+  return text.split("\n").map((line) => line.trim()).join("\n");
+}
+
+function vueifyPlugin()
+{
+  var processStyle = function(done, text, path)
+  {
+    if (!text) {
+      return done();
+    }
+
+    console.log("Processing STYLE...");
+
+    var moduleMapping = null;
+    postcss([
+      require('postcss-modules')({
+        getJSON: function(cssFileName, json) {
+          moduleMapping = json;
+        }
+      })
+    ]).
+    process(text).
+    then(function(result) {
+      var cssObj = new File({
+        contents: new Buffer(result.css),
+        path: path.replace(".vue", ".css")
+      });
+
+      /*
+      var mappingObj = new File({
+        contents: new Buffer(JSON.stringify(moduleMapping)),
+        path: path.replace(".vue", ".css.json")
+      })
+      */
+
+      // We need to write this file directly to have it available for posthtml-css-modules
+      // Unfortunately the API there does not seem to support using a JS object instead.
+      fs.writeFileSync(path.replace(".vue", ".css.json"), JSON.stringify(moduleMapping))
+
+      done(null, cssObj);
+    }).
+    catch(function(ex) {
+      done("Error while transforming style: " + ex);
+    });
+  };
+
+  var minifyTemplate = true;
+
+  var processTemplate = function(done, text, path)
+  {
+    if (!text) {
+      return done();
+    }
+
+    var warnings = templateValidate(text);
+    warnings.forEach((msg) => {
+      console.warn(msg)
+    });
+
+    console.log("Processing TEMPLATE...");
+    posthtml([
+      posthtmlCssModules(path.replace(".vue", ".css.json"))
+    ]).
+    process(text).
+    then((result) => {
+      var html = minifyTemplate ? htmlMinifier.minify(result.html, templateMinifyOptions) : cleanTemplateText(result.html);
+      var htmlObj = new File({
+        contents: new Buffer(html),
+        path: path.replace(".vue", ".html")
+      });
+
+      var js = `export default ${JSON.stringify(html)}`;
+      var jsObj = new File({
+        contents: new Buffer(js),
+        path: path.replace(".vue", ".html.js")
+      });
+
+      done(null, htmlObj, jsObj);
+    }).
+    catch(function(ex) {
+      done("Error while transforming template: ", ex)
+    });
+  };
+
+  var processScript = function(done, text, path)
+  {
+    if (!text) {
+      return done();
+    }
+
+    console.log("Processing SCRIPT...");
+
+    var fileObj = new File({
+      contents: new Buffer(text),
+      path: path.replace(".vue", ".js")
+    });
+
+    done(null, fileObj);
+  }
+
+
+  var transform = function(file, encoding, callback)
+  {
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new util.PluginError("gulp-vuesplit", 'Streams are not supported'));
+      return callback();
+    }
+
+    var content = file.contents.toString('utf8');
+    var fragment = parse5.parseFragment(content, {
+      locationInfo: true
+    });
+    var nodes = convertFragmentIntoNodeMap(fragment);
+    var stream = this;
+    var filePath = file.path;
+    var moduleMapping = null;
+
+    var styleNode = nodes.style;
+    var htmlNode = nodes.html;
+    var scriptNode = nodes.script;
+
+    series(
+    [
+      function(done) {
+        processStyle(done, nodes.style, filePath)
+      },
+      function(done, ...files)
+      {
+        files.forEach((file) => stream.push(file))
+        processTemplate(done, nodes.template, filePath)
+      },
+      function(done, ...files)
+      {
+        files.forEach((file) => stream.push(file))
+        processScript(done, nodes.script, filePath)
+      },
+      function(done, ...files)
+      {
+        files.forEach((file) => stream.push(file))
+        done(null);
+      }
+    ],
+    function(err, results)
+    {
+      if (err)
+      {
+        console.error(err);
+      }
+      else
+      {
+        console.log("ALL DONE")
+        callback();
+      }
+    });
+  };
+
+  return through.obj(transform);
+};
+
