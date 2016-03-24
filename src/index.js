efa223a0b50520f7288e4a20f2e2dffd2d8da9d9
Sebastian Werner <s.werner@sebastian-software.de>
2016-03-24 18:25:16 +0100

    Executed autofix by eslint

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import fs from 'fs';
-import through from 'through2';
-import parse5 from 'parse5';
-import deindent from 'de-indent';
-import File from 'vinyl';
-import postcss from 'postcss';
-import series from "async/series";
-import posthtml from 'posthtml';
-import posthtmlCssModules from 'posthtml-css-modules';
-import templateValidate from 'vue-template-validator';
-import htmlMinifier from 'html-minifier';
+import fs from 'fs'
+import through from 'through2'
+import parse5 from 'parse5'
+import deindent from 'de-indent'
+import File from 'vinyl'
+import postcss from 'postcss'
+import series from "async/series"
+import posthtml from 'posthtml'
+import posthtmlCssModules from 'posthtml-css-modules'
+import templateValidate from 'vue-template-validator'
+import htmlMinifier from 'html-minifier'
 
 
 // required for Vue 1.0 shorthand syntax
 var templateMinifyOptions = {
-  customAttrSurround: [[/@/, new RegExp('')], [/:/, new RegExp('')]],
+  customAttrSurround: [ [ /@/, new RegExp("") ], [ /:/, new RegExp("") ] ],
   collapseWhitespace: true,
   removeComments: true,
   collapseBooleanAttributes: true,
@@ -28,26 +28,26 @@ var templateMinifyOptions = {
 
 function convertFragmentIntoNodeMap(fragment)
 {
-  var nodes = {};
+  var nodes = {}
   fragment.childNodes.forEach(function(child) {
     // Ignore text (typically just white space) and comment nodes
     if (child.nodeName === "#text" || child.nodeName === "#comment") {
-      return;
+      return
     }
 
-    var content = deindent(parse5.serialize(child.content || child)).trim();
-    nodes[child.nodeName] = content;
+    var content = deindent(parse5.serialize(child.content || child)).trim()
+    nodes[child.nodeName] = content
   })
 
-  return nodes;
+  return nodes
 }
 
 function getContentFromNode(node) {
-  return deindent(parse5.serialize(node.content || node));
+  return deindent(parse5.serialize(node.content || node))
 }
 
 function cleanTemplateText(text) {
-  return text.split("\n").map((line) => line.trim()).join("\n");
+  return text.split("\n").map((line) => line.trim()).join("\n")
 }
 
 function vueifyPlugin()
@@ -55,16 +55,16 @@ function vueifyPlugin()
   var processStyle = function(done, text, path)
   {
     if (!text) {
-      return done();
+      return done()
     }
 
-    console.log("Processing STYLE...");
+    console.log("Processing STYLE...")
 
-    var moduleMapping = null;
+    var moduleMapping = null
     postcss([
-      require('postcss-modules')({
+      require("postcss-modules")({
         getJSON: function(cssFileName, json) {
-          moduleMapping = json;
+          moduleMapping = json
         }
       })
     ]).
@@ -73,7 +73,7 @@ function vueifyPlugin()
       var cssObj = new File({
         contents: new Buffer(result.css),
         path: path.replace(".vue", ".css")
-      });
+      })
 
       /*
       var mappingObj = new File({
@@ -86,127 +86,127 @@ function vueifyPlugin()
       // Unfortunately the API there does not seem to support using a JS object instead.
       fs.writeFileSync(path.replace(".vue", ".css.json"), JSON.stringify(moduleMapping))
 
-      done(null, cssObj);
+      done(null, cssObj)
     }).
     catch(function(ex) {
-      done("Error while transforming style: " + ex);
-    });
-  };
+      done("Error while transforming style: " + ex)
+    })
+  }
 
-  var minifyTemplate = true;
+  var minifyTemplate = true
 
   var processTemplate = function(done, text, path)
   {
     if (!text) {
-      return done();
+      return done()
     }
 
-    var warnings = templateValidate(text);
+    var warnings = templateValidate(text)
     warnings.forEach((msg) => {
       console.warn(msg)
-    });
+    })
 
-    console.log("Processing TEMPLATE...");
+    console.log("Processing TEMPLATE...")
     posthtml([
       posthtmlCssModules(path.replace(".vue", ".css.json"))
     ]).
     process(text).
     then((result) => {
-      var html = minifyTemplate ? htmlMinifier.minify(result.html, templateMinifyOptions) : cleanTemplateText(result.html);
+      var html = minifyTemplate ? htmlMinifier.minify(result.html, templateMinifyOptions) : cleanTemplateText(result.html)
       var htmlObj = new File({
         contents: new Buffer(html),
         path: path.replace(".vue", ".html")
-      });
+      })
 
-      var js = `export default ${JSON.stringify(html)}`;
+      var js = `export default ${JSON.stringify(html)}`
       var jsObj = new File({
         contents: new Buffer(js),
         path: path.replace(".vue", ".html.js")
-      });
+      })
 
-      done(null, htmlObj, jsObj);
+      done(null, htmlObj, jsObj)
     }).
     catch(function(ex) {
       done("Error while transforming template: ", ex)
-    });
-  };
+    })
+  }
 
   var processScript = function(done, text, path)
   {
     if (!text) {
-      return done();
+      return done()
     }
 
-    console.log("Processing SCRIPT...");
+    console.log("Processing SCRIPT...")
 
     var fileObj = new File({
       contents: new Buffer(text),
       path: path.replace(".vue", ".js")
-    });
+    })
 
-    done(null, fileObj);
+    done(null, fileObj)
   }
 
 
   var transform = function(file, encoding, callback)
   {
     if (file.isNull()) {
-      return callback(null, file);
+      return callback(null, file)
     }
 
     if (file.isStream()) {
-      this.emit('error', new util.PluginError("gulp-vuesplit", 'Streams are not supported'));
-      return callback();
+      this.emit("error", new util.PluginError("gulp-vuesplit", "Streams are not supported"))
+      return callback()
     }
 
-    var content = file.contents.toString('utf8');
+    var content = file.contents.toString("utf8")
     var fragment = parse5.parseFragment(content, {
       locationInfo: true
-    });
-    var nodes = convertFragmentIntoNodeMap(fragment);
-    var stream = this;
-    var filePath = file.path;
-    var moduleMapping = null;
+    })
+    var nodes = convertFragmentIntoNodeMap(fragment)
+    var stream = this
+    var filePath = file.path
+    var moduleMapping = null
 
-    var styleNode = nodes.style;
-    var htmlNode = nodes.html;
-    var scriptNode = nodes.script;
+    var styleNode = nodes.style
+    var htmlNode = nodes.html
+    var scriptNode = nodes.script
 
     series(
-    [
-      function(done) {
+      [
+        function(done) {
         processStyle(done, nodes.style, filePath)
       },
-      function(done, ...files)
+        function(done, ...files)
       {
         files.forEach((file) => stream.push(file))
         processTemplate(done, nodes.template, filePath)
       },
-      function(done, ...files)
+        function(done, ...files)
       {
         files.forEach((file) => stream.push(file))
         processScript(done, nodes.script, filePath)
       },
-      function(done, ...files)
+        function(done, ...files)
       {
         files.forEach((file) => stream.push(file))
-        done(null);
+        done(null)
       }
-    ],
+      ],
     function(err, results)
     {
       if (err)
       {
-        console.error(err);
+        console.error(err)
       }
       else
       {
         console.log("ALL DONE")
-        callback();
+        callback()
       }
-    });
-  };
+    })
+  }
 
-  return through.obj(transform);
-};
+  return through.obj(transform)
+}
 
--- test.js
+++ test.js
@@ -4,5 +4,5 @@ import "babel-register"
 import plugin from "./src"
 
 test("Dummy", (t) => {
-  t.same(2, 2);
+  t.same(2, 2)
 })
